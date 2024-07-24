1a268651fc9599bcc38024709e1681e3b47c27b6
Robin <138255602+Robin-009@users.noreply.github.com>
2024-07-24 17:25:04 +0000

    547. Number of Provinces

---
+++ 12_java.java
@@ -0,0 +1,59 @@
+// There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+// A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+// You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+// Return the total number of provinces.
+
+class UnionFind {
+  public UnionFind(int n) {
+    count = n;
+    id = new int[n];
+    rank = new int[n];
+    for (int i = 0; i < n; ++i)
+      id[i] = i;
+  }
+
+  public void unionByRank(int u, int v) {
+    final int i = find(u);
+    final int j = find(v);
+    if (i == j)
+      return;
+    if (rank[i] < rank[j]) {
+      id[i] = j;
+    } else if (rank[i] > rank[j]) {
+      id[j] = i;
+    } else {
+      id[i] = j;
+      ++rank[j];
+    }
+    --count;
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  private int count;
+  private int[] id;
+  private int[] rank;
+
+  private int find(int u) {
+    return id[u] == u ? u : (id[u] = find(id[u]));
+  }
+}
+
+class Solution {
+  public int findCircleNum(int[][] isConnected) {
+    final int n = isConnected.length;
+    UnionFind uf = new UnionFind(n);
+
+    for (int i = 0; i < n; ++i)
+      for (int j = i; j < n; ++j)
+        if (isConnected[i][j] == 1)
+          uf.unionByRank(i, j);
+
+    return uf.getCount();
+  }
+}
